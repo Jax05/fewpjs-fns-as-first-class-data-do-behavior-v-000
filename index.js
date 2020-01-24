625a3bcfe1969b3582cee7e39148d57b4219afb4
Carley Tripp <carley.tripp@outlook.com>
2020-01-24 22:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  timeArray = timeString.split(":")
+  hour = timeArray[0]
+}
 /* Write your implementation of displayMessage() */
